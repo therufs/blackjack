a7daaf899b71628e66dc10da977a53af19981911
Ruthan <rcfreese@gmail.com>
2014-06-05 18:59:32 -0400

    Stub out methods in Round, aka annoy Rubocop

---
+++ round.rb
@@ -0,0 +1,19 @@
+# One round of blackjack, from dealing to settling up.
+class Round 
+  
+  def deal
+  end
+  
+  def hit(hand)
+  end
+  
+  def stand  ## seems dumb to stub/write a method that does nothing?
+  end
+  
+  def who_won?(player, dealer)
+  end
+  
+  def payout(pot)
+  end
+  
+end
