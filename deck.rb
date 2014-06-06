a506be81be5ef60c779b90c7814afeed2ef15ebe
Ruthan <rcfreese@gmail.com>
2014-06-06 14:06:04 -0400

    Write initialize and shuffle in deck.rb

---
+++ deck.rb
@@ -0,0 +1,37 @@
+# require 'card' ## pry doesn't like this? 
+
+class Deck
+  def initialize
+    deck = []  ## this name makes me uncomfortable
+    suits = [:clubs, :diamonds, :hearts, :spades]
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K, :A]
+    suits.each do |s|
+      ranks.each do |r|
+        card = Card.new(r, s)  ## it always uses the first arg twice!
+        deck.push card
+      end
+    end
+    @deck = deck
+  end
+  
+  def deck
+    @deck
+  end
+  
+  def shuffle  ## KNUTH SHUFFLE
+    i = 0
+    l = @deck.length
+    @deck.each do
+      j = rand([i..l-1]) ## "no implicit convrsion of array into integer"
+      swap!(deck[i], deck[j])
+      i += 1
+    end
+    @deck
+  end
+  
+  def swap!(a, b)
+    self[a], self[b] = self[b], self[a]
+    self
+  end
+  
+end
